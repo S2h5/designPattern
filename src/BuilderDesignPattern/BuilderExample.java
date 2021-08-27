cdea4d6d5df59bf4f8eeaa73a293809b408caee4
s2h5 <shreyahansda@gmail.com>
2021-08-27 12:01:50 +0530

    Second Commit

---
+++ src/BuilderDesignPattern/BuilderExample.java
@@ -0,0 +1,20 @@
+package BuilderDesignPattern;
+
+public class BuilderExample {
+
+    
+        
+        public static void main(String[] args) {
+
+            Phone.Builder builder = new Phone.Builder()
+                    .backPanel("Sandstone")
+                    .frontPanel("AMOLED")
+                    .camera("12 MP")
+                    .processor("Snapdragon 625");
+    
+            Phone phone = builder.build();
+    
+            System.out.println(phone);
+         }
+    
+}
